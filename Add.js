2053e0131cbba0f0d6366dc78b7eea4d47f1eec0
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-04-09 21:19:19 +0530

    added solution 2 for Add.js

--- Add.js
+++ Add.js
@@ -1,9 +1,19 @@
+//solution one
 function AddUpTo(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
-    total += n;
+    total += i;
   }
   return total;
 }
 
 console.log(AddUpTo(6));
+
+///------------------------------
+
+//solution two
+function AddUpToSimple(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(AddUpToSimple(6));
